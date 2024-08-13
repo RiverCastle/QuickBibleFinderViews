2dd01f6c5f51cd5d08b77c2394b921d83147c08b
RiverCastle <rkdtjd12@gmail.com>
2024-08-13 14:10:08 +0900

    first commit

---
+++ src/main/java/JesusDisciples/QuickBibleFinderViews/QuickBibleFinderViewsApplication.java
@@ -0,0 +1,13 @@
+package JesusDisciples.QuickBibleFinderViews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickBibleFinderViewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickBibleFinderViewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JesusDisciples/QuickBibleFinderViews/QuickBibleFinderViewsApplicationTests.java
@@ -0,0 +1,13 @@
+package JesusDisciples.QuickBibleFinderViews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickBibleFinderViewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
