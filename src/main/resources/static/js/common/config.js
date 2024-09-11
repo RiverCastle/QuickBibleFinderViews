e27607072b0070c5ccf0ba4e8f1cf2908e3f61fb
RiverCastle <rkdtjd12@gmail.com>
2024-09-11 12:31:10 +0900

    hotfix: 프론트엔드 url add

--- src/main/resources/static/js/common/config.js
+++ src/main/resources/static/js/common/config.js
@@ -1,2 +1,2 @@
 const BACKEND_BASE_URL = 'http://ec2-13-209-42-193.ap-northeast-2.compute.amazonaws.com:8080';
-const FRONTEND_BASE_URL = 'http://localhost:8080'
+const FRONTEND_BASE_URL = 'ec2-43-203-222-159.ap-northeast-2.compute.amazonaws.com';
