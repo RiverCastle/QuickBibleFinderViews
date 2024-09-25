8dd801367a07331f0efc3ba0f1f05a8ba6f3f12d
RiverCastle <rkdtjd12@gmail.com>
2024-09-25 16:31:27 +0900

    feat: DNS connection

--- src/main/resources/static/js/common/config.js
+++ src/main/resources/static/js/common/config.js
@@ -1,2 +1,2 @@
-const BACKEND_BASE_URL = 'http://ec2-13-209-42-193.ap-northeast-2.compute.amazonaws.com:8080';
-const FRONTEND_BASE_URL = 'http://quick-bible-finder.site:8080';
+const BACKEND_BASE_URL = 'http://quick-bible-finder.site:8080';
+const FRONTEND_BASE_URL = 'http://quick-bible-finder.site';
